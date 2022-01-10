babd0bd6298e30c0bfa91f8a51c46f61cc49b839
kolaso13 <adrbrarare@gmail.com>
2022-01-10 22:15:17 +0100

    otrocommit

--- scripts/script.js
+++ scripts/script.js
@@ -8,9 +8,25 @@ L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {maxZoom: 18}).
 
 for(i=0;i < aValizas.length;i++){
     var Valizas = L.marker([aValizas[i].GpxY, aValizas[i].GpxX]).addTo(Mapa);
-    Valizas.bindPopup(`<b>${aValizas[i].Nombre}</b>`).openPopup();
+    Valizas.bindPopup(`${aValizas[i].Id}`);
+    Valizas.on("click", Agregar);
 }
 
+function Agregar(e) {
+    var ImprimirDiv = "";
+    var sObtenerID = e.target.getPopup().getContent();
+    console.log(sObtenerID);
 
+    for(i=0; i < aValizas.length;i++){
+        if(aValizas[i].Id == sObtenerID){
+            sID = aValizas[i].Id;
+
+                ImprimirDiv =`<div id="opcion${sID}">
+                                <p>${aValizas[i].Nombre}</p>
+                            </div>`;    
+        }
+    }
+    document.getElementById("Contenido").innerHTML = ImprimirDiv;
+}
 
 
