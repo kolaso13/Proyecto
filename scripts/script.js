6e3f215f472d150957ec3065f2ce54ca6e5e13e8
kolaso13 <adrbrarare@gmail.com>
2022-01-11 10:46:36 +0100

    ColocacionCorrecta

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,13 @@
 import L from 'leaflet';
 
 var aValizas = JSON.parse(sValizas);
+var aId = new Array();
+var bexiste;
+var Mapa = L.map('map').setView([43.34578351332376, -1.7965434243182008], 11);
 
-var Mapa = L.map('map').setView([43.34578351332376, -1.7965434243182008],11);
+L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { maxZoom: 18 }).addTo(Mapa);
 
-L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {maxZoom: 18}).addTo(Mapa);
-
-for(i=0;i < aValizas.length;i++){
+for (i = 0; i < aValizas.length; i++) {
     var Valizas = L.marker([aValizas[i].GpxY, aValizas[i].GpxX]).addTo(Mapa);
     Valizas.bindPopup(`${aValizas[i].Id}`);
     Valizas.on("click", Agregar);
@@ -14,19 +15,31 @@ for(i=0;i < aValizas.length;i++){
 
 function Agregar(e) {
     var ImprimirDiv = "";
+
     var sObtenerID = e.target.getPopup().getContent();
-    console.log(sObtenerID);
-
-    for(i=0; i < aValizas.length;i++){
-        if(aValizas[i].Id == sObtenerID){
-            sID = aValizas[i].Id;
-
-                ImprimirDiv =`<div id="opcion${sID}">
-                                <p>${aValizas[i].Nombre}</p>
-                            </div>`;    
+    for (i = 0; i < aId.length; i++) {
+        if (aId[i] == sObtenerID) {
+            bexiste = true;
+            break;
+        }
+        else {
+            bexiste = false;
         }
     }
-    document.getElementById("Contenido").innerHTML = ImprimirDiv;
+    console.log(sObtenerID);
+    if (!bexiste) {
+        aId.push(sObtenerID);
+        for (i = 0; i < aValizas.length; i++) {
+            if (aValizas[i].Id == sObtenerID) {
+                sID = aValizas[i].Id;
+                ImprimirDiv += `<div class="tarjetas col" id="opcion${sID}">
+                                <p>${aValizas[i].Nombre}</p>
+                            </div>`;    
+            }
+        }
+
+        document.getElementById("Contenido").innerHTML += ImprimirDiv;
+    }
 }
 
 
