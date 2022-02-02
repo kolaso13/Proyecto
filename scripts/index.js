cfa18b015538863fbbbf346e874eb18368251518
kolaso13 <adrbrarare@gmail.com>
2022-02-02 13:05:03 +0100

    ComentariosPuestos

--- scripts/index.js
+++ scripts/index.js
@@ -50,32 +50,38 @@ var GreenIcon = new L.Icon({
     shadowSize: [41, 41]
 });
 
+//Añadimos onClick en los botones
 document.getElementById("btnAcceso").addEventListener("click", Acceso)
 document.getElementById("btnCerrarSesion").addEventListener("click", CerrarSesion);
-Token();
 
+//Llamamos a la funcion que confirma si tenemos token (nos lleva a la pagina del tiempo sin necesidad de loguearse) en caso de no tenerlo no tendremos que loguear
+Token();
 function Token(){
+    //Hacemos un fetch a la API del tiempo con el JWToken que tengamos 
     fetch("https://localhost:5001/api/Tiempo", {
         headers: {
             'Content-Type': 'application/json',
             'Authorization': `Bearer ${localStorage.getItem("JWTtoken")}`,
         }
     }).then(response => {
+    //si el token es correcto
       if (response.ok) {
         $("#TrozoMapa").css("display", "block");
         ObtencionDeDatosAPI();
+    //si el token es incorrecto
       }else{
         $("#TrozoLogin").css("display", "block");
       }
     })
 }
 
-
+//Funcion que borra el token y actualiza la pagina
 function CerrarSesion(){
     localStorage.removeItem("JWTtoken");
     location.reload();
 }
 
+//Funcion para Iniciar sesion con los datos introducidos en el formulario
 function Acceso() {
   fetch("https://localhost:5001/Users/authenticate", {
       method: "POST",
@@ -86,10 +92,12 @@ function Acceso() {
           "username": $("#typeUsernameX-2").val(),
           "password": $("#typePasswordX-2").val(),
       }),
+      //Responde con el JSON (usuario, contraseña, token...)
   }).then(response => {
         if (response.ok) {
-            return response.json()
+            return response.json();
         }
+        //Añadimos al localstorage el token y ocultamos el login y mostramos el mapa y llamamos a la funcion que añade los datos
       }).then(e => {
           localStorage.setItem("JWTtoken", e.token)
           $("#TrozoLogin").css("display", "none");
@@ -99,7 +107,7 @@ function Acceso() {
           console.log("Usuario o contraseña incorrectos");
       });
 }
-
+//Funcion que obtiene los datos de la API del tiempo
 function ObtencionDeDatosAPI() {
     fetch("https://localhost:5001/api/Tiempo", {
         headers: {
@@ -107,9 +115,11 @@ function ObtencionDeDatosAPI() {
             'Authorization': `Bearer ${localStorage.getItem("JWTtoken")}`,
         }
     }).then(response => response.json()).then(aBalizas => {
+        //Creamos el mapa
         var Mapa = L.map('map').setView([43.34578351332376, -1.7965434243182008], 11);
         L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { maxZoom: 18 }).addTo(Mapa);
 
+        //Funcion que crea las balizas segun los datos recibidos
         CrearBalizas();
         function CrearBalizas() {
             //Creamos la balizas
@@ -118,7 +128,7 @@ function ObtencionDeDatosAPI() {
                 Balizas.bindPopup(`${aBalizas[i].nombre}`);
                 Balizas.on("click", Agregar);
                 HoverPopUp(Balizas);
-
+                //Cambiamos el color de las balizas segun su tipo
                 if (aBalizas[i].tipoEstacion == "BUOY") {
                     Balizas.setIcon(RedIcon);
                 } else if (aBalizas[i].tipoEstacion == "METEOROLOGICAL") {
@@ -128,18 +138,16 @@ function ObtencionDeDatosAPI() {
                 } else {
                     Balizas.setIcon(GreenIcon);
                 }
-
                 aMarcadores.push(Balizas);
             }
         }
-        //Funcion que agrega un div cuando se hace clic en un marcador
+
+        //Funcion que agrega una tarjeta cuando se hace clic en un marcador
         function Agregar(e) {
-            console.log("Es el this " + e);
             var sImprimirDiv = "";
 
             //Cogemos el nombre de las balizas
             var sObtenerNombre = e.target.getPopup().getContent();
-            console.log(sObtenerNombre);
 
             //Con el nombre cogemos el id
             for (var i = 0; i < aBalizas.length; i++) {
@@ -148,7 +156,8 @@ function ObtencionDeDatosAPI() {
                     break;
                 }
             }
-            //Comprobamos que el ID seleccionado no esta ya
+
+            //Comprobamos que el ID seleccionado no esta ya en el array
             if (aId.length < 4) {
                 for (var i = 0; i < aId.length; i++) {
                     if (aId[i] == sObtenerID) {
@@ -159,7 +168,7 @@ function ObtencionDeDatosAPI() {
                         bExiste = false;
                     }
                 }
-                console.log(sObtenerID);
+
                 //Si no existe imprimimos una tarjeta y añadimos al array de los IDs el nuevo
                 if (!bExiste) {
                     //Cambiamos el color
@@ -168,6 +177,7 @@ function ObtencionDeDatosAPI() {
                     //Añadimos al localstorage el array
                     localStorage.IDs = JSON.stringify(aId);
 
+                    //Imprimimos la tarjeta con sus datos
                     for (var i = 0; i < aBalizas.length; i++) {
                         if (aBalizas[i].id == sObtenerID) {
                             var sID = aBalizas[i].id;
@@ -217,6 +227,8 @@ function ObtencionDeDatosAPI() {
             }
             Jquery(aBalizas);
         }
+
+        //Imprimimos el select para el fltro
         var sImprimirSelect = `<select id="select" name="select">
                         <option value="Todos">Todos</option>
                         <option value="Bizkaia">Bizkaia</option>
@@ -225,15 +237,23 @@ function ObtencionDeDatosAPI() {
                         </select>
                         `;
         document.getElementById("Provincias").innerHTML = sImprimirSelect;
-        console.log(aMarcadores);
+
+        //Creamos el onChange para que el select, al seleccionar una nueva opcion ejecute lo siguiente
         $("select").on("change", function () {
+            //Cogemos el valor del select
             var sProvinciaSeleccionada = document.getElementById("select").value;
+            
+            //Limpiamos el mapa
             aMarcadores.forEach(i => {
                 Mapa.removeLayer(i);
             });
+
             aMarcadores = [];
+
+            //Si seleccionamos todo creamos las balizas normalmente
             if (sProvinciaSeleccionada == "Todos") {
                 CrearBalizas();
+            //Si hemos seleccionamos otra opcion solo añadimos las que tengan la provincia deseada
             } else {
                 for (var i = 0; i < aBalizas.length; i++) {
                     if (aBalizas[i].provincia == sProvinciaSeleccionada) {
@@ -250,25 +270,9 @@ function ObtencionDeDatosAPI() {
                         }
                         HoverPopUp(Balizas);
                         aMarcadores.push(Balizas);
-
-                    } else {
-                        let Balizas = L.marker([aBalizas[i].gpxY, aBalizas[i].gpxX], { idMarcador: aBalizas[i].id }).bindPopup(`${aBalizas[i].nombre}`);
-                        if (aBalizas[i].tipoEstacion == "BUOY") {
-                            Balizas.setIcon(RedIcon);
-                        } else if (aBalizas[i].tipoEstacion == "METEOROLOGICAL") {
-                            Balizas.setIcon(BlueIcon);
-                        } else if (aBalizas[i].tipoEstacion == "GAUGING") {
-                            Balizas.setIcon(YellowIcon);
-                        } else {
-                            Balizas.setIcon(GreenIcon);
-                        }
-                        HoverPopUp(Balizas);
-                        aMarcadores.push(Balizas);
                     }
                 }
-
             }
-            
             //Cambiamos la baliza a color negro
             for (var i = 0; i < aId.length; i++) {
                 for (var j = 0; j < aMarcadores.length; j++) {
@@ -281,6 +285,8 @@ function ObtencionDeDatosAPI() {
         RevisarLocalStorage(aBalizas);
     })
 };
+
+//Funcion para que las balizas tengan un hover al pasar el raton por encima
 function HoverPopUp(Balizas){
     Balizas.on("mouseover", function (e) {
         this.openPopup();
@@ -290,14 +296,18 @@ function HoverPopUp(Balizas){
         this.closePopup();
     });
 }
-function Jquery(aBalizas) {
-    $(document).ready(function () {
-        $("svg").on("click", function () {
-            $("#Provincias").slideToggle(750);
-            $("#map").slideToggle(750);
-            $("#colores").slideToggle(750);
-        });
+//Jquery para minimizar el mapa
+$(document).ready(function () {
+    $("svg").on("click", function () {
+        $("#Provincias").slideToggle(750);
+        $("#map").slideToggle(750);
+        $("#colores").slideToggle(750);
     });
+});
+
+//Funcion que agrupa varios tipos de JQuery
+function Jquery(aBalizas) {
+
     //Apareceran al cargar la pagina
     $(".MostrarPrincipio").addClass("Mostrar");
 
@@ -316,7 +326,7 @@ function Jquery(aBalizas) {
         //Volvemos a subir el array con el id eliminado
         localStorage.IDs = JSON.stringify(aId);
 
-        //Cambiamos el icono negro por el azul cuando se cierra la tarjeta
+        //Cambiamos el icono negro por el original cuando se cierra la tarjeta
         for (var i = 0; i < aMarcadores.length; i++) {
             if (aMarcadores[i].options.idMarcador == id) {
                 if (aBalizas[i].tipoEstacion == "BUOY") {
@@ -328,10 +338,9 @@ function Jquery(aBalizas) {
                 } else {
                     aMarcadores[i].setIcon(GreenIcon);
                 }
-                console.log("Cambio");
             }
         }
-        //La clase mas cercana a la x la elimina
+        //La clase tarjetas mas cercana a la x la elimina
         $(this).closest(".tarjetas").remove();
     })
 
@@ -372,8 +381,11 @@ function RevisarLocalStorage(aBalizas) {
     document.getElementById("Contenido").innerHTML = "";
     var sImprimirLocalStorage = "";
     var aNombre = new Array();
+
+    //Si no hay localstorage se crea
     if (localStorage.IDs == undefined) {
         IDs = [];
+        localStorage.IDs = JSON.stringify(IDs);
         var allaves = new Array();
     } else {
         //Cogemos el array del localstorage
